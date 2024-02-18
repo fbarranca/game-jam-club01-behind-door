ccf70de244a2e61edee94675bc2e7b990b472991
Franco Barranca <FBARRANCA01@mynbcc.ca>
2024-02-17 21:29:00 -0400

    Reset score
    when loading the game scene, reset score

--- DoorsKnocker/Assets/Scripts/Controllers/GameController.cs
+++ DoorsKnocker/Assets/Scripts/Controllers/GameController.cs
@@ -14,6 +14,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ScoreController.ResetScore();
         doors = FindObjectsOfType<Door>();
         timer.onTimerEnd.AddListener(EndGame);
     }
